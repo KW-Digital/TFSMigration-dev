feb5872610148845a282f677689c29bcef0983fd
Andreas Bergström <andreas.bergstrom@kw-digital.com>
2014-04-03 14:27:47 +0000

    Brached
    git-tfs-id: [https://kw-digital.visualstudio.com/Defaultcollection]$/TFS Migration/Migration/TSFMigrationSample-dev;C7243

---
+++ TSFMigrationSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSFMigrationSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Edited in kw-digital.visualstudio.com
+            // And edited in ab-kwd-test.visualstudio.com
+
+            // EDIT 1
+            // EDIT 3
+            Console.WriteLine("Success!");
+
+            Console.ReadLine();
+        }
+    }
+}
